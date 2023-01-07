1283ec07833af24a0171a420f0bb4b694ba87a37
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-01-07 11:15:20 -0300

    atualizei ex014

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -1,19 +1,23 @@
 function carregar(){
     var msg = document.getElementById('msg')
+    var msg2 = document.getElementById('msg2')
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png' //var img tem ocaminho declarado  em document.getElementById...
         document.body.style.background = '#c7b182'
-    } else if (hora >= 12 && hora <= 18){
-        //BOA TARDE!
+        msg2.innerHTML = 'Bom dia!'
+    } else if (hora >= 12 && hora <= 18){ 
         img.src = 'fototarde.png'
         document.body.style.background = '#c68277'
+        msg2.innerHTML = 'Boa tarde!'
     } else {
-        //BOA NOITE!
+        
         img.src = 'fotonoite.png'
         document.body.style.background = '#112e53'
+        msg2.innerHTML = 'Boa noite!'
     }
 }
