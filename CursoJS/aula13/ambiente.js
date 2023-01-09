365eccde1ed300307f9fcb9e0ac93aaf281f2c28
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-01-09 20:33:38 -0300

    Estruturas de repetições

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,31 @@
+for (var c = 1; c<=10 ; c++){
+    console.log (`Passo ${c}`)
+}
+
+
+
+/*LAÇO COM DO / WHILE
+
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while ( c <= 6)
+*/
+
+/* LAÇO COM WHILE
+var c = 1
+while (c <= 6){
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
