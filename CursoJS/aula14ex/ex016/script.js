19cd98caf87f60dfc065e09f71c5be170f16e659
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-01-09 21:24:30 -0300

    Ex repetição proposto

---
+++ CursoJS/aula14ex/ex016/script.js
@@ -0,0 +1,12 @@
+function contar(){
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.querySelector('#res')
+    
+    while (Number(inicio.value) <= Number(fim.value)){
+        
+        (Number(inicio.value) + Number(passo.value))
+    }
+    res.innerHTML=(`${passo}`)
+}
