fc3ad81b3a34a8bd24542f941002abdae5460b5a
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-01-11 20:18:49 -0300

    ex de repetições

--- CursoJS/aula14ex/ex016/script.js
+++ CursoJS/aula14ex/ex016/script.js
@@ -2,11 +2,24 @@ function contar(){
     var inicio = document.getElementById('inicio')
     var fim = document.getElementById('fim')
     var passo = document.getElementById('passo')
-    var res = document.querySelector('#res')
-    
-    while (Number(inicio.value) <= Number(fim.value)){
-        
-        (Number(inicio.value) + Number(passo.value))
+    var res = document.getElementById('res')
+
+    if (inicio.value == '' || fim.value == '' || passo.value == 0){
+        window.alert ('[ERRO] Faltam dados!')
+    } else {
+        var i = Number(inicio.value) //conversão das variáveis de string para numbers
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        res.innerHTML = 'Contando: <br>'
+        if (i < f){ //CONTAGEM PROGRESSIVA
+            for (var c = i; c <= f; c = c + p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else { // CONTAGEM REGRESSIVA
+            for (var c=i; f <= c; c=c-p){
+                res.innerHTML +=`${c} \u{1F449}`
+            }
+        }
+        res.innerHTML +=`${c} \u{1F3C1}`//como o comando de resultado é igual, é só colocar uma vez fora do if ao invés de colocar duas vezes.
     }
-    res.innerHTML=(`${passo}`)
 }
--- CursoJS/aula14ex/ex017/script.js
+++ CursoJS/aula14ex/ex017/script.js
@@ -1,1 +1,32 @@
-alert('Olá!')
+function contar(){
+    var num = document.getElementById('txtnum')
+    var res = document.querySelector('#res')
+
+    if (num.value == ''){
+        window.alert ('[ERRO] Verifique seus dados e tente novamente!')
+    } else {
+        var n = Number(num.value)
+        var t = 1
+        /* FORMA COM O FOR
+        for(var t=1;t<=10;t++){
+        res.innerHTML+=`${n} x ${t} = ${t*n} <br>`
+        }
+        */ 
+        /*FORMA COM O WHILE
+        while (t <= 10){
+            res.innerHTML+=`${n} x ${t} = ${t*n} <br>`
+            t++
+       }
+       */
+       // FORMA COM O WHILE, MOSTRADO NO OPTION CRIADO.
+       res.innerHTML= ''
+       while(t <= 10){
+        let item = document.createElement('option') //option é o elemento no HTML que compõe a tag select, para cada option, é criado um item
+        item.text = `${n} x ${t} = ${t*n}`
+        res.appendChild(item) //para mostrar o item criado.
+        t++
+       }
+
+    }
+
+}
