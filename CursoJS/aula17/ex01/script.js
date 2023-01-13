229ebe5cea0918ff568b743d8492e8f72e5dc8f0
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-01-13 20:43:06 -0300

    atualizei exercício de JS

--- CursoJS/aula17/ex01/script.js
+++ CursoJS/aula17/ex01/script.js
@@ -13,7 +13,7 @@ function isNum(n){
 }
 
 function inLista(n, l){
-    if (l.indexOf(Number(n)) != -1){
+    if (l.indexOf(Number(n)) != -1){ //o indexOf retorna o valor onde o n está, e o -1 indica que o valor não está na lista (aula de array), e o != é negação. Então lê-se assim: retorna o valor de n caso ele NÃO ESTEJA na lista ( != -1)
         return true
     } else {
         return false
@@ -21,10 +21,45 @@ function inLista(n, l){
 }
 
 function adicionar(){
-    //primeiro limitar se o que foi digitado é um número e se repetiu
+    //primeiro limitar se o que foi digitado é um número e se repetiu na lista
     if (isNum(num.value) && !inLista(num.value, valores)){
-        window.alert('Tudo ok')
+        valores.push(Number(num.value)) // vai adicionar os números digitados na variável num ao vetor
+        //agora temos que adicionar o valor na lista (select), para isso precisamos de um <option> 
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado` 
+        lista.appendChild(item) // o appendChild é necessário para mostrar o item criado, n
+        res.innerHTML = ''
+
     } else{
         window.alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = '' //para depois de adicionar um número campo ficar vazio
+    num.focus() //para o cursos do mouse ficar posicionado (focado) no campo num
+
+}
+
+function finalizar(){
+    if (valores.length == 0){
+    window.alert('[ERRO] Primeiro adicione valores!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        soma = 0
+        media = 0
+        for (let pos in valores){ // criada var pos para verificar > e < números
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML =''
+        res.innerHTML +=`<p> Ao todo temos ${tot} número(s) cadastrado(s)</p>`
+        res.innerHTML +=`<p>O maior valor é o ${maior}</p>`
+        res.innerHTML +=`<p>O menor valor é o ${menor}</p>`
+        res.innerHTML +=`<p>A soma dos valores digitados é ${soma}</p>`
+        res.innerHTML +=`<p>A média dos valores digitados é ${media}</p>`
     }
 }
