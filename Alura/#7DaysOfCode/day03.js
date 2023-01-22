65d4b9e6498d8dde09c653db024843d4273dd6a2
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-01-22 18:24:10 -0300

    Finalizei dia 3 #7DaysOfCode

--- Alura/#7DaysOfCode/day03.js
+++ Alura/#7DaysOfCode/day03.js
@@ -1,12 +1,29 @@
-function dia03() {
+var nome = prompt('Qual o seu nome?')
+var linguagem = prompt('Qual linguagem de programação você está estudando?')
 
-var nome = document.querySelector('#txtnome')
-var ling = document.querySelector('#txtling')
-var res = document.querySelector('#res')
+var perg1 = prompt(`Olá ${nome}, já que você está aprendendo ${linguagem}, qual área você pretende seguir? Front-end [1] ou Back-end? [2]`)
 
-if(nome.textContent == '' || ling.textContent ==''){
-    alert('Por favor, preenchar os campos acima antes de continuar!')
+if (perg1 == '1'){
+    var perg2 = prompt('Legal! No Front-End você quer aprender React[1] ou Vue[2]?')
+} else (
+    prompt('Legal! No Back-End você quer aprender C# [1] ou Java [2]?')
+)
+
+var perg3 = prompt("Legal, você pretende se especializar na área[1] ou vai seguir estudando para se tornar Fullstack[2]?")
+if (perg3 == '1'){
+    alert('Continue seus estudos para se aprofundar na sua área!')
 } else {
-    res.innerHTML=(`Olá, ${nome}, já que você está estudando ${ling}, qual área você quer seguir, Front-End ou Back-End?`)
-    }
+    alert('Continue seus estudos para se tornar Fullstack!')
 }
+
+alert("Provavelmente existem tecnologias que você gostaria de conhecer ou se especilizar, conte para nós quais são elas")
+
+//estrutura de repetição com string
+var tec = [perg4]
+do {
+    var perg4 = prompt('Quais tecnologias você quer aprender ou se especializar? Caso não tiver mais nenhuma em mente, digite: [2] para finalizar')
+    tec.push(perg4)
+} while (perg4 !== '2')
+
+console.log(tec)
+
---
+++ CursoJS/praticas/arrays.js
@@ -0,0 +1,10 @@
+for (var c = 0; c < tec.length ; c++){
+var perg4 = prompt('Quais tecnologias você quer aprender ou se especializar? Caso não tiver mais nenhuma em mente, digite: Sair')
+    if (perg4 === 'Sair'){
+        console.log('ok')
+    } else {
+    var tec = [perg4]
+    }
+}
+
+
