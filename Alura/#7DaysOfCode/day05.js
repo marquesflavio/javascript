990aff75c06b10d9c4a731b070c988cc4a3f4cd8
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-01-24 22:58:15 -0300

    Editei #7DaysOfCode - day5

--- Alura/#7DaysOfCode/day05.js
+++ Alura/#7DaysOfCode/day05.js
@@ -2,30 +2,30 @@ let arrayFrutas = ['maçã']
 let arrayVerduras = []
 let arrayLaticinios = ['leite']
 let arrayBebidas = []
-let listaDeCompras = [arrayBebidas, arrayFrutas, arrayLaticinios, arrayVerduras]
 
 for (c = 0 ; c < 10; c++){
 let res = prompt('Deseja adicionar algo na sua lista de compras, sim ou não?')
     if (res == 'não'){
-        console.log(`${listaDeCompras}`)
         break
     } else {
         let item = prompt('Qual item você quer adicionar?')
-        let categoria = prompt('Qual categoria se encaixa?')
+        let categoria = prompt('Qual categoria se encaixa? Frutas, Verduras, Laticinios ou Bebidas?')
         switch(categoria){
-            case ('frutas'):
+            case ('Frutas'):
                 arrayFrutas.push(item)
                 break 
-            case ('verduras'):
+            case ('Verduras'):
                 arrayVerduras.push(item)
                 break 
-            case ('laticinios'):
+            case ('Laticinios'):
                 arrayLaticinios.push(item)
                 break 
-            case ('bebidas'):
+            case ('Bebidas'):
                 arrayBebidas.push(item)
                 break 
             
         }
     } 
-} console.log(`${listaDeCompras}`)
+} 
+var lista = document.querySelector('.lista-mercado')
+lista.innerHTML = (`Frutas:${arrayFrutas}<br> Verduras:${arrayVerduras}<br> Bebidas:${arrayBebidas}<br> Laticínios: ${arrayLaticinios}.`)
