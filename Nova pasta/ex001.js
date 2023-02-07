204b48ba6be8dae63f1d86701e69c0ee72cc6545
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-02-06 21:12:19 -0300

    atualização de pastas

--- Nova pasta/ex001.js
+++
@@ -1,6 +0,0 @@
-function palindromo (){
-    var palavra = "rever"
-    var separandoLetras = palavra.split('') //split separa cada letra da string
-    var palavraInvertida = separandoLetras.reverse()
-    palavraInvertida = palavraInvertida.join('')
-}
